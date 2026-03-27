9532916196dab28770e7a23a9e6a48cbd1f30da7
kashioapp <contact@kashioapp.com>
2026-03-27 00:17:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 function toggleSidebar(){
-document.querySelector(".sidebar").classList.toggle("collapsed");
+document.getElementById("sidebar").classList.toggle("collapsed");
 }
 
 function toggleChat(){
-document.querySelector(".chat-box").classList.toggle("open");
+document.getElementById("chatBox").classList.toggle("open");
 }
 
 function sendMessage(){
-const msg = document.getElementById("chatInput").value;
 
+const input = document.getElementById("chatInput");
+const msg = input.value;
+
+if(!msg) return;
+
+const chat = document.getElementById("chatMessages");
+
+/* show message */
+const div = document.createElement("div");
+div.className = "user-msg";
+div.innerText = msg;
+chat.appendChild(div);
+
+/* send to webhook */
 fetch("https://discord.com/api/webhooks/1486913651712724992/nK_fkyLouoC1FXHeLKXIH_XyBjSI_2k5RzcILzOcNpW38khjkqpqctAQtKz-v6RKLyog", {
 method:"POST",
 headers:{"Content-Type":"application/json"},
-body:JSON.stringify({content:msg})
+body:JSON.stringify({
+content: "Support Message: " + msg
+})
 });
+
+input.value = "";
+chat.scrollTop = chat.scrollHeight;
 }
