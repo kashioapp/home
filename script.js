ce645061cb6dc278782080c66077d74b6d524774
kashioapp <contact@kashioapp.com>
2026-03-27 00:45:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,26 @@
-let supportOnline = false;
-
-/* SIDEBAR */
 function toggleSidebar(){
-const sidebar = document.getElementById("sidebar");
-const logo = document.getElementById("logo");
-
-sidebar.classList.toggle("collapsed");
-
-if(sidebar.classList.contains("collapsed")){
-logo.src = "./favicon.png";
-} else {
-logo.src = "./bannerlogo.png";
-}
+document.getElementById("sidebar").classList.toggle("collapsed");
 }
 
-/* SEARCH */
 function toggleSearch(){
 document.getElementById("searchBox").classList.toggle("active");
 }
 
-/* CHAT */
 function toggleChat(){
-const chat = document.getElementById("chat");
-chat.classList.toggle("open");
+document.getElementById("chat").classList.toggle("open");
 
-const status = document.getElementById("chatStatus");
-
-if(supportOnline){
-status.innerText = "Online";
-} else {
-status.innerText = "Offline - contact@kashioapp.com";
-}
+document.getElementById("chatStatus").innerText =
+"Offline - contact@kashioapp.com";
 }
 
-/* SEND MESSAGE */
 function sendMsg(){
 const input = document.getElementById("msgInput");
-const text = input.value;
-if(!text) return;
-
-const box = document.getElementById("chatMessages");
+if(!input.value) return;
 
 const msg = document.createElement("div");
-msg.innerText = text;
-box.appendChild(msg);
+msg.innerText = input.value;
+
+document.getElementById("chatMessages").appendChild(msg);
 
 input.value="";
 }
